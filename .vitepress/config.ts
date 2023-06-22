0d5560bb77c2273c3a3538d0c8e60d51ac7a38a7
Hugo Alliaume <hugo@alliau.me>
2023-06-22 10:17:08 +0200

    chore: setup layouts + posts loading

---
+++ .vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Hugo Alliaume",
+  description: "My Personal Blog",
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/site.webmanifest" }],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: '#000000' }],
+    ['meta', { name: "msapplication-TileColor", content: "#000000" }],
+    ['meta', { name: "theme-color", content: "#000000" }],
+    ['script', {
+      async: "true",
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-Z8KN175TJZ',
+    }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-Z8KN175TJZ');`,
+    ],
+  ]
+})
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import Home from "./Home.vue";
+import NotFound from "./NotFound.vue";
+import Post from "./Post.vue";
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, page, frontmatter } = useData()
+</script>
+
+<template>
+  <header>
+      The header
+  </header>
+
+  <main>
+      <Home v-if="frontmatter.layout === 'home'" />
+      <NotFound v-else-if="page.isNotFound" />
+      <Post v-else />
+  </main>
+
+  <footer>
+      The footer
+  </footer>
+</template>
