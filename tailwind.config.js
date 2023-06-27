c85c17f891940da1bff6f3fb0e592ca43469d637
Hugo Alliaume <hugo@alliau.me>
2023-06-27 22:56:34 +0200

    fix(tailwind): use .dark CSS class for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./posts/**/*.md", ".vitepress/theme/**/*.vue"],
+    darkMode: ['class', '.dark'],
     theme: {
         screens: {
             'sm': '640px',
