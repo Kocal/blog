578c3595dcb05e00cdd91d0ccf4d021c3515c0fe
Hugo Alliaume <hugo@alliau.me>
2023-11-11 00:17:57 +0100

    chore(netlify): add config file and redirections

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -58,6 +58,7 @@ export default defineConfig({
   lastUpdated: true,
   head: [
     ['link', { rel: 'sitemap', type: 'application/xml', href: '/sitemap.xml' }],
+    ['link', { rel: 'alternate', type: 'application/rss+xml', href: '/rss.xml' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
